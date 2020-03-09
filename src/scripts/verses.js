ae5faed4b66334f43d5ab43789daa622b88fa692
julieg18 <juliannagal18@gmail.com>
2020-03-09 07:43:09 -0500

    Refactor Code - refactored verses.js to improve code readability

--- src/scripts/verses.js
+++ src/scripts/verses.js
@@ -3,77 +3,12 @@ import 'normalize.css/normalize.css';
 import '../styles/style.css';
 import '../styles/verses.css';
 
+const verses = document.querySelector('.page__verses');
 const currentUrl = new URL(window.location.href);
 const currentEmotion = currentUrl.searchParams.get('emotion');
-const verses = document.querySelector('.page__verses');
 const currentEmotionReferences = verseReferences[currentEmotion];
 
-document.title = `When You're Feeling ${currentEmotion[0].toUpperCase() +
-  currentEmotion.substring(1)}`;
-
-document.querySelector(
-  '.page__title',
-).innerHTML = `When You're Feeling ${currentEmotion[0].toUpperCase() +
-  currentEmotion.substring(1)}`;
-
-function copyVerseToClipboard(e) {
-  const verseReference = e.srcElement.parentNode.querySelector(
-    '.verse__reference',
-  ).innerText;
-  const versePassage = e.srcElement.parentNode.parentNode.querySelector(
-    '.verse__passage',
-  ).innerText;
-  const verse = `${versePassage} - ${verseReference}`;
-
-  const textArea = document.createElement('textarea');
-  textArea.value = verse;
-  textArea.setAttribute('readOnly', '');
-  textArea.style.position = 'absolute';
-  textArea.style.left = '-9999px';
-
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textArea);
-
-  const copyButtons = document.querySelectorAll('.verse__copy-button');
-  copyButtons.forEach((button) => {
-    if (button !== e.srcElement) {
-      button.innerHTML = 'Copy';
-    }
-  });
-  e.srcElement.innerHTML = 'Copied!';
-}
-
-function createVerseHtml(verseIdNum, verseRef, verseContent) {
-  const verseHeader = document.createElement('div');
-  const versePassage = document.createElement('div');
-  const verseReference = document.createElement('p');
-  const verseCopyBtn = document.createElement('button');
-  const verse = document.querySelector(`.verse-${verseIdNum}`);
-
-  verseHeader.className = 'verse__header';
-  versePassage.className = 'verse__passage';
-  verseReference.className = 'verse__reference';
-  verseCopyBtn.className = 'verse__copy-button';
-
-  verseReference.innerHTML = verseRef;
-  verseCopyBtn.innerHTML = 'Copy';
-  versePassage.innerHTML = verseContent;
-
-  verseCopyBtn.addEventListener('click', copyVerseToClipboard);
-
-  verseHeader.appendChild(verseReference);
-  verseHeader.appendChild(verseCopyBtn);
-
-  verse.innerHTML = '';
-  verse.appendChild(verseHeader);
-  verse.appendChild(versePassage);
-}
-
-function createLoadingVerse(verseIdNum) {
-  const loadingVerse = document.createElement('li');
-  loadingVerse.className = `verse verse-${verseIdNum}`;
+function createLoadingAnimation() {
   const loadingAnimation = document.createElement('div');
   loadingAnimation.className = 'loading-animation';
   for (let i = 1; i < 4; i += 1) {
@@ -81,16 +16,12 @@ function createLoadingVerse(verseIdNum) {
     loadingDot.className = `loading-animation__dot loading-animation__dot_num_${i}`;
     loadingAnimation.appendChild(loadingDot);
   }
-  loadingVerse.appendChild(loadingAnimation);
-  return loadingVerse;
+  return loadingAnimation;
 }
 
-async function addVerse(verseRef, verseIdNum) {
-  const loadingVerse = createLoadingVerse(verseIdNum);
-  verses.appendChild(loadingVerse);
-
+async function getVerse(verseRef) {
   const url = new URL(
-    `https://api.scripture.api.bible/v1/bibles/06125adad2d5898a-01/passages/${verseRef}`,
+    `https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/passages/${verseRef}`,
   );
   const queryParams = {
     'content-type': 'text',
@@ -110,8 +41,95 @@ async function addVerse(verseRef, verseIdNum) {
   const {
     data: { reference, content },
   } = parsedReq;
-  createVerseHtml(verseIdNum, reference, content);
+  return { reference, content };
 }
+
+function changeCopyButtonsText(copyButtonId) {
+  const copyButtons = document.querySelectorAll('.verse__copy-button');
+  const lastClickedCopyButton = document.querySelector(`.${copyButtonId}`);
+
+  copyButtons.forEach((button) => {
+    if (button !== lastClickedCopyButton) {
+      button.innerHTML = 'Copy';
+    }
+  });
+  lastClickedCopyButton.innerHTML = 'Copied!';
+}
+
+function copyVerseToClipboard(e) {
+  const verseReference = e.srcElement.parentNode.querySelector(
+    '.verse__reference',
+  ).innerText;
+  const versePassage = e.srcElement.parentNode.parentNode.querySelector(
+    '.verse__passage',
+  ).innerText;
+  const copyButtonId = e.srcElement.classList[1];
+  const verse = `${versePassage} - ${verseReference}`;
+
+  const textArea = document.createElement('textarea');
+  textArea.value = verse;
+  textArea.setAttribute('readOnly', '');
+  textArea.style.position = 'absolute';
+  textArea.style.left = '-9999px';
+
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+
+  changeCopyButtonsText(copyButtonId);
+}
+
+function createVerse(verseIdNum, verseRef, verseContent) {
+  const verseHeader = document.createElement('div');
+  const versePassage = document.createElement('div');
+  const verseReference = document.createElement('p');
+  const verseCopyBtn = document.createElement('button');
+
+  verseHeader.className = 'verse__header';
+  versePassage.className = 'verse__passage';
+  verseReference.className = 'verse__reference';
+  verseCopyBtn.className = `verse__copy-button verse__copy-button_id_${verseIdNum}`;
+
+  verseReference.innerHTML = verseRef;
+  verseCopyBtn.innerHTML = 'Copy';
+  versePassage.innerHTML = verseContent;
+
+  verseCopyBtn.addEventListener('click', copyVerseToClipboard);
+
+  verseHeader.appendChild(verseReference);
+  verseHeader.appendChild(verseCopyBtn);
+
+  return { verseHeader, versePassage };
+}
+
+async function addVerse(verseRef, verseIdNum) {
+  const loadingVerse = document.createElement('li');
+  loadingVerse.className = `verse verse-${verseIdNum}`;
+  const loadingAnimation = createLoadingAnimation();
+  loadingVerse.appendChild(loadingAnimation);
+  verses.appendChild(loadingVerse);
+
+  const retrievedVerse = await getVerse(verseRef);
+  const { reference, content } = retrievedVerse;
+
+  const { verseHeader, versePassage } = createVerse(
+    verseIdNum,
+    reference,
+    content,
+  );
+  loadingVerse.innerHTML = '';
+  loadingVerse.appendChild(verseHeader);
+  loadingVerse.appendChild(versePassage);
+}
+
+document.title = `When You're Feeling ${currentEmotion[0].toUpperCase() +
+  currentEmotion.substring(1)}`;
+
+document.querySelector(
+  '.page__title',
+).innerHTML = `When You're Feeling ${currentEmotion[0].toUpperCase() +
+  currentEmotion.substring(1)}`;
 
 for (let i = 0; i < currentEmotionReferences.length; i += 1) {
   addVerse(currentEmotionReferences[i], i + 1);
