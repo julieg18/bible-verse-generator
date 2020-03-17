f8d70c2bb07d91e7d0465f589dbb3cb50eb4f975
julieg18 <juliannagal18@gmail.com>
2020-03-17 06:09:29 -0500

    install autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
